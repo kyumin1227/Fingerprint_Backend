54c8006e244bb6afcaa704b84d18fd2e9d29a095
kyumin1227 <kyumin12271227@gmail.com>
2024-04-18 22:35:00 +0900

    Setting: FingerprintBackend Setting

---
+++ src/main/java/com/example/fingerprint_backend/FingerprintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fingerprint_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingerprintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FingerprintBackendApplication.class, args);
+	}
+
+}
