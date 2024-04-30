e7037a41d8b8ab3a79cbf700b081eba05f3b3aef
kyumin1227 <kyumin12271227@gmail.com>
2024-04-30 10:22:46 +0900

    Setting: Front-server Cors Allow

--- src/main/java/com/example/fingerprint_backend/FingerprintBackendApplication.java
+++ src/main/java/com/example/fingerprint_backend/FingerprintBackendApplication.java
@@ -2,12 +2,25 @@ package com.example.fingerprint_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FingerprintBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FingerprintBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+			}
+		};
 	}
 
 }
