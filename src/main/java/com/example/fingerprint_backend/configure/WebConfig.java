5b5afeff8ca0deabb45e62504e38cafce042c676
kyumin1227 <kyumin12271227@gmail.com>
2024-05-15 13:39:42 +0900

    Test: Cors 허용을 위해 config 설정

---
+++ src/main/java/com/example/fingerprint_backend/configure/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.fingerprint_backend.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://ec2-52-78-122-31.ap-northeast-2.compute.amazonaws.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
