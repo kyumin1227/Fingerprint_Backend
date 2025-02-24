ab543bf725fa7fdaa758e53a83cddd1e241225b6
kyumin1227 <kyumin12271227@gmail.com>
2025-02-24 20:47:16 +0900

    Feat: 메소드 자동화
    - 스케줄 생성, 그룹 생성, 스케줄 완료 자동화

---
+++ src/main/java/com/example/fingerprint_backend/scheduled/CleanScheduled.java
@@ -0,0 +1,95 @@
+package com.example.fingerprint_backend.scheduled;
+
+import com.example.fingerprint_backend.entity.CleanArea;
+import com.example.fingerprint_backend.entity.CleanGroup;
+import com.example.fingerprint_backend.entity.CleanSchedule;
+import com.example.fingerprint_backend.repository.CleanAreaRepository;
+import com.example.fingerprint_backend.service.CleanOperationService;
+import com.example.fingerprint_backend.service.CleanScheduleGroupService;
+import jakarta.transaction.Transactional;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+public class CleanScheduled {
+
+    private final CleanAreaRepository cleanAreaRepository;
+    private final CleanScheduleGroupService cleanScheduleGroupService;
+    private final CleanOperationService cleanOperationService;
+
+    public CleanScheduled(CleanAreaRepository cleanAreaRepository, CleanScheduleGroupService cleanScheduleGroupService, CleanOperationService cleanOperationService) {
+        this.cleanAreaRepository = cleanAreaRepository;
+        this.cleanScheduleGroupService = cleanScheduleGroupService;
+        this.cleanOperationService = cleanOperationService;
+    }
+
+    @Scheduled(cron = "0 0 12 * * ?")
+    @Transactional
+    /**
+     * 자동으로 청소 스케줄을 생성하는 메소드
+     */
+    public void createScheduleIfNeeded() {
+        LocalDate today = LocalDate.now();
+        List<CleanArea> areas = cleanAreaRepository.findAll();
+
+        for (CleanArea cleanArea : areas) {
+            List<CleanSchedule> schedules = cleanScheduleGroupService.getScheduleByAreaNameAndSchoolClassName(cleanArea.getName(), cleanArea.getSchoolClass().getName(), today);
+            if (schedules.size() < cleanArea.getDisplay()) {
+                cleanScheduleGroupService.createCleanSchedules(
+                        cleanArea.getLastScheduledDate(),
+                        cleanArea.getName(),
+                        cleanArea.getSchoolClass().getName(),
+                        cleanArea.getCycle(),
+                        cleanArea.getDays(),
+                        cleanArea.getDisplay() - schedules.size()
+                );
+            }
+        }
+    }
+
+    @Scheduled(cron = "0 0 13 * * ?")
+    @Transactional
+    /**
+     * 자동으로 청소 그룹을 생성하는 메소드
+     */
+    public void createGroupIfNeeded() {
+        List<CleanArea> areas = cleanAreaRepository.findAll();
+
+        for (CleanArea cleanArea : areas) {
+            List<CleanGroup> groups = cleanScheduleGroupService.getGroupsByAreaNameAndClassNameAndIsCleaned(cleanArea.getName(), cleanArea.getSchoolClass().getName(), false);
+            while (groups.size() < cleanArea.getDisplay()) {
+                cleanScheduleGroupService.createGroupsByRandom(
+                        cleanArea.getName(),
+                        cleanArea.getSchoolClass().getName(),
+                        cleanArea.getMembers(),
+                        cleanArea.getGroupSize()
+                );
+            }
+        }
+    }
+
+    @Scheduled(cron = "0 0 14 * * ?")
+    @Transactional
+    /**
+     * 자동으로 청소 스케줄을 완료하는 메소드
+     */
+    public void completeScheduleIfNeeded() {
+        LocalDate today = LocalDate.now();
+        int minusDays = 1;
+        List<CleanArea> areas = cleanAreaRepository.findAll();
+
+        for (CleanArea cleanArea : areas) {
+            List<CleanSchedule> schedules = cleanScheduleGroupService.getScheduleByAreaNameAndSchoolClassName(cleanArea.getName(), cleanArea.getSchoolClass().getName(), today.minusDays(minusDays));
+            for (CleanSchedule schedule : schedules) {
+                if (schedule.isCanceled()) {
+                    continue;
+                }
+                cleanOperationService.completeCleaningSchedule(today.minusDays(minusDays), cleanArea.getName(), cleanArea.getSchoolClass().getName());
+            }
+        }
+    }
+
+}
