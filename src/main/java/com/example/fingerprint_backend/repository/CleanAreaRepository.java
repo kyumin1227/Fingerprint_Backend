c6fc8dcb8e24922df28c3870831ecabc08713de0
kyumin1227 <kyumin12271227@gmail.com>
2025-04-15 01:46:45 +0900

    Style: 불필요한 Repository 어노테이션 제거

--- src/main/java/com/example/fingerprint_backend/repository/CleanAreaRepository.java
+++ src/main/java/com/example/fingerprint_backend/repository/CleanAreaRepository.java
@@ -3,11 +3,9 @@ package com.example.fingerprint_backend.repository;
 import com.example.fingerprint_backend.entity.CleanArea;
 import com.example.fingerprint_backend.entity.SchoolClass;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface CleanAreaRepository extends JpaRepository<CleanArea, Long> {
     boolean existsByNameAndSchoolClass(String name, SchoolClass schoolClass);
 
--- src/main/java/com/example/fingerprint_backend/repository/LogRepository.java
+++ src/main/java/com/example/fingerprint_backend/repository/LogRepository.java
@@ -3,12 +3,10 @@ package com.example.fingerprint_backend.repository;
 import com.example.fingerprint_backend.entity.LogEntity;
 import com.example.fingerprint_backend.types.LogAction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-@Repository
 public interface LogRepository extends JpaRepository<LogEntity, Long> {
     Optional<LogEntity> findByStudentNumberAndActionAndEventTimeAfter(
             String studentNumber,
